1d26485df77cd94d359fda61ca97d2a72ad6f5ec
taytom258 <3746203+taytom258@users.noreply.github.com>
2026-08-09 01:21:03 -0400

    Scripted commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,7 +9,7 @@ cd /data
 cp /data/start_server.sh /data/start_server_local.sh
 if [[ $server_name == "Dedicated Server" ]] || [[ $server_pass == "change_me" ]]; then
 	echo For your security, please set the server name and password in /data/run.sh before restarting the server...
-	exit 3
+	exit 0
 fi
 sed -i "s#./valheim_server.x86_64.*#./valheim_server.x86_64 -batchmode -nographics -name ${server_name} -port 2456 -world ${server_world} -password ${server_pass} -savedir /data/worlds -preset normal -crossplay#g" \
 /data/start_server_local.sh
