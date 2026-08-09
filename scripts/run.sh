08bbceb4c6dbe3317d879a698b32ec614b1dc267
taytom258 <3746203+taytom258@users.noreply.github.com>
2026-08-09 01:11:54 -0400

    Scripted commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 date=`date +"%Y-%m-%d-%H%M"`
-#Place your game server specific run instructions below here
+server_name="Dedicated Server"
+server_pass="change_me"
+server_world="Dedicated"
 
 cd /data
 cp /data/start_server.sh /data/start_server_local.sh
-touch /data/logs/$date-server.log
-sed -i 's#./valheim_server.x86_64.*#./valheim_server.x86_64 -batchmode -nographics -name "Dedicated Server" -port 2456 -world "Dedicated" -password "change_me" -savedir /data/worlds -preset normal -crossplay#g' \
+if [[ $server_name == "Dedicated Server" ]] || [[ $server_pass == "change_me" ]]; then
+	echo For your security, please set the server name and password in /data/run.sh before restarting the server...
+	exit 3
+fi
+sed -i "s#./valheim_server.x86_64.*#./valheim_server.x86_64 -batchmode -nographics -name ${server_name} -port 2456 -world ${server_world} -password ${server_pass} -savedir /data/worlds -preset normal -crossplay#g" \
 /data/start_server_local.sh
 
 /bin/bash /data/start_server_local.sh @*
